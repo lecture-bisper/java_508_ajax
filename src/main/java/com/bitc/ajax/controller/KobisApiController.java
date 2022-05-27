75f0069472b2566e4f04d4f21d441a6bd32ca266
lecture-bisper <bisper@outlook.kr>
2022-05-27 12:18:11 +0900

    영화진흥원 오픈API 컨트롤러 수정.

--- src/main/java/com/bitc/ajax/controller/KobisApiController.java
+++ src/main/java/com/bitc/ajax/controller/KobisApiController.java
@@ -2,6 +2,7 @@ package com.bitc.ajax.controller;
 
 import com.bitc.ajax.dto.Covid19InfoItemDto;
 import com.bitc.ajax.dto.DailyBoxOfficeDto;
+import com.bitc.ajax.dto.DailyBoxOfficeJsonDto;
 import com.bitc.ajax.service.Covid19InfoService;
 import com.bitc.ajax.service.KobisService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,49 +11,52 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 public class KobisApiController {
 
-    @Value("${user.kobis.key}")
-    private String secKey;
+  @Value("${user.kobis.key}")
+  private String secKey;
 
-    @Autowired
-    private KobisService kobisService;
+  @Autowired
+  private KobisService kobisService;
 
 
-    @RequestMapping("/kobis/dailyBoxOfficeList")
-    public String kobisDailyBoxOfficeList() throws Exception {
-        return "kobis/dailyBoxOfficeList";
-    }
+  @RequestMapping("/kobis/dailyBoxOfficeList")
+  public String kobisDailyBoxOfficeList() throws Exception {
+    return "/kobis/dailyBoxOfficeList";
+  }
 
-    @RequestMapping(value = "/kobis/dailyBoxOfficeListXml", method = RequestMethod.POST)
-    public Object kobisDailyBoxOfficeListXml(@RequestParam("targetDt") String targetDt) throws Exception {
-        String endPoint = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/";
-        String serviceFunc = "searchDailyBoxOfficeList.xml?";
-        String serviceKey = "key=";
-        String opt1 = "&targetDt=";
+  @ResponseBody
+  @RequestMapping(value = "/kobis/dailyBoxOfficeListXml", method = RequestMethod.POST)
+  public List<DailyBoxOfficeDto> kobisDailyBoxOfficeListXml(@RequestParam("targetDt") String targetDt) throws Exception {
+    String endPoint = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/";
+    String serviceFunc = "searchDailyBoxOfficeList.xml?";
+    String serviceKey = "key=";
+    String opt1 = "&targetDt=";
 
-        String serviceUrl = endPoint + serviceFunc + serviceKey  + secKey + opt1 + targetDt.replace( "-", "");
+    String serviceUrl = endPoint + serviceFunc + serviceKey  + secKey + opt1 + targetDt.replace( "-", "");
 
-        List<DailyBoxOfficeDto> itemList = kobisService.getItemListUrl(serviceUrl, "xml");
+    List<DailyBoxOfficeDto> itemList = (List<DailyBoxOfficeDto>) kobisService.getItemListUrl(serviceUrl, "xml");
 
-        return itemList;
-    }
+    return itemList;
+  }
 
-    @RequestMapping(value = "/kobis/dailyBoxOfficeListJson", method = RequestMethod.POST)
-    public Object kobisDailyBoxOfficeListJson(@RequestParam("targetDt") String targetDt) throws Exception {
-        String endPoint = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/";
-        String serviceFunc = "searchDailyBoxOfficeList.json?";
-        String serviceKey = "key=";
-        String opt1 = "&targetDt=";
+  @ResponseBody
+  @RequestMapping(value = "/kobis/dailyBoxOfficeListJson", method = RequestMethod.POST)
+  public Object kobisDailyBoxOfficeListJson(@RequestParam("targetDt") String targetDt) throws Exception {
+    String endPoint = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/";
+    String serviceFunc = "searchDailyBoxOfficeList.json?";
+    String serviceKey = "key=";
+    String opt1 = "&targetDt=";
 
-        String serviceUrl = endPoint + serviceFunc + serviceKey  + secKey + opt1 + targetDt.replace( "-", "");
+    String serviceUrl = endPoint + serviceFunc + serviceKey  + secKey + opt1 + targetDt.replace( "-", "");
 
-        List<DailyBoxOfficeDto> itemList = kobisService.getItemListUrl(serviceUrl, "json");
+    List<DailyBoxOfficeJsonDto> itemList = (List<DailyBoxOfficeJsonDto>) kobisService.getItemListUrl(serviceUrl, "json");
 
-        return itemList;
-    }
+    return itemList;
+  }
 }
